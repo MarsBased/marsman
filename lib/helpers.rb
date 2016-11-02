5f0ffbc4d70166e7f58bb2a96d8c5e112b94d539
Javier Artero <javier.artero@gmail.com>
2016-11-02 16:18:13 +0100

    [RB] Added split to helpers

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,6 +2,7 @@ module Helpers
 
   def active(name, class_name = 'active', reverse = false)
     if(class_active = current_page.data.active)
+      class_active.split(/[\s,']/)
       if(reverse)
         class_name if(!class_active.include?(name))
       else
@@ -12,6 +13,7 @@ module Helpers
 
   def modify(name, return_value = true )
     if(modify = current_page.data.modify)
+      modify.split(/[\s,']/)
       return_value if(modify.include?(name))
     end
   end
