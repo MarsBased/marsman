d95b3afde2917ef2a4da4c568ab3c2c1e2ab1d41
Javier Artero <javier.artero@gmail.com>
2017-05-09 19:58:04 +0200

    Fix internal links and relative files path in config.rb (#37)
    * Replace internal link a to link_to and fix path to source
    
    * Fix file path to relative and ignore hash assets

--- config.rb
+++ config.rb
@@ -1,11 +1,14 @@
 require "lib/helpers"
 helpers Helpers
 
+set :relative_links, true
+
 activate :sprockets
 activate :autoprefixer
 activate :pry
 activate :directory_indexes
 activate :sprockets3_sassc
+activate :relative_assets
 
 configure :development do
   activate :livereload, no_swf: true
@@ -29,8 +32,9 @@ end
 
 configure :build do
   ignore 'shapes/*'
-  activate :asset_hash
+  activate :asset_hash, ignore: /assets/
   set :environment, 'production'
+  #set :http_prefix, '/marsman'
 end
 
 activate :deploy do |deploy|
